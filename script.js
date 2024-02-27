aaa4db7270e276e4dfcb71dfb217e887bc33d38b
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-02-27 17:41:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
 //your code here
+let list = document.querySelector('ul');
+list.classList = "band";
+// console.log(list);
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let arr = [];
+touristSpots.forEach((spot) =>{
+	let str = "";
+	spot.toLowerCase().split(" ").forEach((index) => {
+		if(index === "the" || index === "a" || index === "an"){
+			str +=  "";
+		}else {
+			str += index + " ";
+		}
+	})
+	arr.push(str);
+})
+
+arr = arr.sort();
+// console.log(arr);
+arr.forEach((index) => {
+	  // console.log("index" ,index)
+	  let li = document.createElement("li");
+	  li.appendChild(document.createTextNode(index));
+	  list.appendChild(li);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
